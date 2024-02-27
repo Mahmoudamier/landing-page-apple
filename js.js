1bd56e0a297c323c36058cb27ac988d44f97ec02
Mahmoud Amier <89768108+Mahmoudamier@users.noreply.github.com>
2024-02-27 20:37:46 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".container")
+function phones(source) {
+    img.src = source;
+}
+function colors(color){
+    container.style.background = color;
+}
